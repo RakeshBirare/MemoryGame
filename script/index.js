8c7e72558145fca9b665d7a1f4245eacea6f0df0
Rakesh Birare <rakeshbirare9@gmail.com>
2021-10-09 16:21:07 +0530

    Almost Complit
    designing part of the project is remainig but i will do it some onother day

---
+++ script/index.js
@@ -0,0 +1,59 @@
+// Grab Elements First
+const cards = document.querySelectorAll('.card');
+cards.forEach((card) => card.addEventListener("click",flip))
+
+// variables 
+var isFlipped = false;
+var firstCard;
+var secondCard;
+
+function flip() {
+    this.classList.add("flip");
+    if (!isFlipped) {
+        isFlipped = true;
+        firstCard = this;
+    }else {
+        secondCard = this;
+        checkIt();
+    }
+}
+
+function checkIt(){
+    if(firstCard.dataset.image === secondCard.dataset.image) {
+        success();
+    } else {
+        fail();
+    }
+}
+
+function success() {
+    firstCard.addEventListener('click',flip)
+    secondCard.addEventListener('click',flip)
+    reset();
+}
+
+function fail() {
+    setTimeout(() => {
+        firstCard.classList.remove("flip")
+        secondCard.classList.remove("flip")
+        reset();
+    }, 500);
+}
+
+function reset() {
+    isFlipped = false;
+    firstCard = null;
+    secondCard = null;
+}
+
+
+
+// todo: shuffle
+
+(function shuffle(){
+    cards.forEach((card) => {
+        var index = Math.floor(Math.random()*16)
+        card.style.order = index
+    })
+})();
+
